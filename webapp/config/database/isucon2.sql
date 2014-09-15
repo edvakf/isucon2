a6096f51597a2b8aad5ec3687792059615fbfa60
Keiji, Yoshimi <walf443@gmail.com>
2014-09-15 15:12:04 +0900

    DBの変更を反映

--- webapp/config/database/isucon2.sql
+++ webapp/config/database/isucon2.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS isucon2;
 CREATE DATABASE IF NOT EXISTS isucon2 DEFAULT CHARACTER SET 'utf8';
 
 -- CREATE USER 'isucon2app'@'%' IDENTIFIED BY 'isunageruna';
@@ -11,21 +12,22 @@ CREATE TABLE IF NOT EXISTS isucon2.artist (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+) ENGINE=Memory;
 
 CREATE TABLE IF NOT EXISTS isucon2.ticket (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `artist_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+) ENGINE=Memory;
 
 CREATE TABLE IF NOT EXISTS isucon2.variation (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `ticket_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+  PRIMARY KEY (`id`),
+  INDEX ticket_id (ticket_id)
+) ENGINE=Memory;
 
 CREATE TABLE IF NOT EXISTS isucon2.stock (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -35,11 +37,11 @@ CREATE TABLE IF NOT EXISTS isucon2.stock (
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `variation_seat` (`variation_id`,`seat_id`)
-) ENGINE=InnoDB;
+) ENGINE=Memory;
 
 CREATE TABLE IF NOT EXISTS isucon2.order_request (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `member_id` VARCHAR(32) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `member_order` (`member_id`)
-) ENGINE=InnoDB;
+) ENGINE=Memory;
